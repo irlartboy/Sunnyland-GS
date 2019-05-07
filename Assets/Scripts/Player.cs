ad572076da19570f94f3b7be6b1fd1d86b602306
irlartboy <ollie_king99@outlook.com>
2019-05-07 11:55:27 +1000

    1st push

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Prime31;
+
+public class Player : MonoBehaviour
+{
+    public float gravity = -10;
+    public float moveSpeed = 10f;
+
+    private CharacterController2D controller;
+   
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController2D>();
+    }
+
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+        controller.move(transform.right * inputH * moveSpeed * Time.deltaTime);
+
+    }
+}
