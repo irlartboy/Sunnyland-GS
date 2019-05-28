676dda59a93b0f04167dbc8067c6ad1d9a4a65a0
irlartboy <ollie_king99@outlook.com>
2019-05-28 12:08:49 +1000

    animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,18 +10,36 @@ public class Player : MonoBehaviour
     public float moveSpeed = 10f;
 
     private CharacterController2D controller;
+    private Animator anim;
+    private SpriteRenderer spriteRend;
    
 
     void Start()
     {
         controller = GetComponent<CharacterController2D>();
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         float inputH = Input.GetAxis("Horizontal");
         float inputV = Input.GetAxis("Vertical");
+        Run(inputH);
+        Climb(inputV);
+
+    }
+    private void Run(float inputH)
+    {
         controller.move(transform.right * inputH * moveSpeed * Time.deltaTime);
+        bool isRunning = inputH != 0;
+        anim.SetBool("isRunning", isRunning);
+
+        spriteRend.flipX = inputH < 0;
+    }
+
+    void Climb(float inputV)
+    {
 
     }
 }
